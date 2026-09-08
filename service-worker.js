464b688ffc71846fa65696ead059d32f37e51719
M3N3F2 <143553667+M3N3F2@users.noreply.github.com>
2026-09-08 07:30:31 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+
+const CACHE = "meter-ledger-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
+    const copy = resp.clone();
+    caches.open(CACHE).then(c => c.put(event.request, copy)).catch(()=>{});
+    return resp;
+  }).catch(() => caches.match("./index.html"))));
+});
